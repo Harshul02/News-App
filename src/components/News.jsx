28ae5708de4944bbd43b9ca69a86ddc71b4af4df
Harshul Jain <harshulgangwal02@gmail.com>
2022-11-18 23:56:01 +0530

    Added card design

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,11 +2,28 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor(){
+    super();
+    console.log("Hello I am a constructor");
+    this.state = {
+      loading: false,
+    }
+  }
   render() {
     return (
-      <div>
-        This is a News Component.
-        <NewsItem />
+      <div className="container my-3">
+        <h2>My News - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" description="MyDescription" imageUrl="" newsUrl="TODO" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" description="MyDescription" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" description="MyDescription" />
+          </div>
+        </div>
       </div>
     )
   }
