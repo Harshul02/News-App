d9cc54e3a307442a7d15068a5f2f3a7fcc0b244e
Harshul Jain <harshulgangwal02@gmail.com>
2022-11-20 00:57:35 +0530

    Added Previous and Next button

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -9,19 +9,21 @@ export class News extends Component {
     console.log("Hello I am a constructor");
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page:1
     }
   }
 
   componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey="+secret;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${secret}&page=1&pageSize=20`;
     // let data = await fetch(url);
     fetch(url)
     .then((res) => res.json())
     .then((json) => {
       this.setState({
           articles: json.articles,
-          loading: true
+          loading: true,
+          totalResults: json.totalResults
       });
       console.log(json.articles);
   });
@@ -31,6 +33,43 @@ export class News extends Component {
     //   articles: parsedData.articles,
     //   // loading: true
     // }
+  }
+
+  handleNextClick = ()=> {
+    // console.log("Next");
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults /20))
+    {
+
+    }
+    else{
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${secret}&page=${this.state.page + 1}&pageSize=20`;
+    // let data = await fetch(url);
+    fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      this.setState({
+          articles: json.articles,
+          loading: true,
+          page: this.state.page + 1
+      })});
+
+    }
+    // this.setState = {
+    //   page: this.state.page + 1
+    // }
+  }
+  handlePrevClick = ()=> {
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${secret}&page=${this.state.page - 1}&pageSize=20`;
+    // let data = await fetch(url);
+    fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      this.setState({
+          articles: json.articles,
+          loading: true,
+          page: this.state.page - 1
+      })});
   }
 
   render() {
@@ -46,19 +85,23 @@ export class News extends Component {
         {articles.map((element)=>{
           let desc = element.description;
           let shortTitle = element.title;
-          if(desc && desc.length >60)
+          {/* if(desc && desc.length >60)
           {
             desc=desc.slice(0,60);
           }
           if(shortTitle && shortTitle.length>45)
           {
             shortTitle = shortTitle.slice(0,45);
-          }
+          } */}
         return( <div className="col-md-4" key={element.url}>
             <NewsItem title={shortTitle} description={desc} imageUrl={element.urlToImage} newsUrl={element.url} />
           </div>);
         })}
           
+        </div>
+        <div className="container d-flex justify-content-between my-4">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -6,10 +6,10 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://images.unsplash.com/photo-1585829365295-ab7cd400c167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">
               Go somewhere
             </a>
